ebcef2ba6b7d0646425d4c6ff9e5a5546ce87a0b
Gangstaaaaa <85770558+Gangstaaaaa@users.noreply.github.com>
2024-06-13 00:41:11 -0400

    tests

--- resources/[mythic]/mythic-hud/server/server.lua
+++ resources/[mythic]/mythic-hud/server/server.lua
@@ -119,63 +119,63 @@ function RegisterChatCommands()
 		help = "Test Blindfold",
 	})
 
-	-- Chat:RegisterAdminCommand("notif", function(source, args, rawCommand)
-	-- 	exports["mythic-base"]:FetchComponent("Execute"):Client(source, "Notification", "Success", "This is a test, lul")
-	-- end, {
-	-- 	help = "Test Notification",
-	-- })
+	Chat:RegisterAdminCommand("notif", function(source, args, rawCommand)
+		exports["mythic-base"]:FetchComponent("Execute"):Client(source, "Notification", "Success", "This is a test, lul")
+	end, {
+		help = "Test Notification",
+	})
 
-	-- Chat:RegisterAdminCommand("list", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("ListMenu:Client:Test", source)
-	-- end, {
-	-- 	help = "Test List Menu",
-	-- })
+	Chat:RegisterAdminCommand("list", function(source, args, rawCommand)
+		TriggerClientEvent("ListMenu:Client:Test", source)
+	end, {
+		help = "Test List Menu",
+	})
 
-	-- Chat:RegisterAdminCommand("input", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Input:Client:Test", source)
-	-- end, {
-	-- 	help = "Test Input",
-	-- })
+	Chat:RegisterAdminCommand("input", function(source, args, rawCommand)
+		TriggerClientEvent("Input:Client:Test", source)
+	end, {
+		help = "Test Input",
+	})
 
-	-- Chat:RegisterAdminCommand("confirm", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Confirm:Client:Test", source)
-	-- end, {
-	-- 	help = "Test Confirm Dialog",
-	-- })
+	Chat:RegisterAdminCommand("confirm", function(source, args, rawCommand)
+		TriggerClientEvent("Confirm:Client:Test", source)
+	end, {
+		help = "Test Confirm Dialog",
+	})
 
-	-- Chat:RegisterAdminCommand("skill", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Minigame:Client:Skillbar", source)
-	-- end, {
-	-- 	help = "Test Skill Bar",
-	-- })
+	Chat:RegisterAdminCommand("skill", function(source, args, rawCommand)
+		TriggerClientEvent("Minigame:Client:Skillbar", source)
+	end, {
+		help = "Test Skill Bar",
+	})
 
-	-- Chat:RegisterAdminCommand("scan", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Minigame:Client:Scanner", source)
-	-- end, {
-	-- 	help = "Test Scanner",
-	-- })
+	Chat:RegisterAdminCommand("scan", function(source, args, rawCommand)
+		TriggerClientEvent("Minigame:Client:Scanner", source)
+	end, {
+		help = "Test Scanner",
+	})
 
-	-- Chat:RegisterAdminCommand("sequencer", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Minigame:Client:Sequencer", source)
-	-- end, {
-	-- 	help = "Test Sequencer",
-	-- })
+	Chat:RegisterAdminCommand("sequencer", function(source, args, rawCommand)
+		TriggerClientEvent("Minigame:Client:Sequencer", source)
+	end, {
+		help = "Test Sequencer",
+	})
 
-	-- Chat:RegisterAdminCommand("keypad", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Minigame:Client:Keypad", source)
-	-- end, {
-	-- 	help = "Test Keypad",
-	-- })
+	Chat:RegisterAdminCommand("keypad", function(source, args, rawCommand)
+		TriggerClientEvent("Minigame:Client:Keypad", source)
+	end, {
+		help = "Test Keypad",
+	})
 
-	-- Chat:RegisterAdminCommand("scrambler", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Minigame:Client:Scrambler", source)
-	-- end, {
-	-- 	help = "Test Scrambler",
-	-- })
+	Chat:RegisterAdminCommand("scrambler", function(source, args, rawCommand)
+		TriggerClientEvent("Minigame:Client:Scrambler", source)
+	end, {
+		help = "Test Scrambler",
+	})
 
-	-- Chat:RegisterAdminCommand("memory", function(source, args, rawCommand)
-	-- 	TriggerClientEvent("Minigame:Client:Memory", source)
-	-- end, {
-	-- 	help = "Test Memory",
-	-- })
+	Chat:RegisterAdminCommand("memory", function(source, args, rawCommand)
+		TriggerClientEvent("Minigame:Client:Memory", source)
+	end, {
+		help = "Test Memory",
+	})
 end
